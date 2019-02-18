21d43ea4e224a51e0fbd4a48c58ed1c71c357200
ClaudineAfflick <claudine.afflick@mymona.uwi.edu>
2019-02-18 01:01:25 +0000

    ContactForm

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from flask_mail import Mail
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8d90b3065fda6c334a382c637c6ad189'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = '465' # (or try 2525)
+app.config['MAIL_USERNAME'] = '1c589a197b8864'
+app.config['MAIL_PASSWORD'] = '1ff0f158179cbe'
+mail = Mail(app)
 from app import views
--- app/views.py
+++ app/views.py
@@ -7,6 +7,9 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from .forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 
 ###
@@ -24,7 +27,24 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-
+@app.route('/contact/',methods=['GET', 'POST'])
+def contact():
+    form=ContactForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            name = form.name.data
+            email = form.email.data
+            subject= form.subject.data
+            textarea=form.textarea.data
+            msg = Message(subject,
+            sender=(name,email),
+            recipients=["to@example.com"])
+            msg.body = textarea
+            mail.send(msg)
+            flash('Message Sent')  
+            return redirect(url_for('home'))
+        flash_errors(form)
+    return render_template('contact.html',form=form)
 ###
 # The functions below should be applicable to all Flask apps.
 ###
